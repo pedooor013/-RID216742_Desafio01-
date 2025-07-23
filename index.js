f20cb96db5c8a342b8d3c4c447146547f6d8950e
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-07-23 17:22:46 -0300

    Criação do front end, começo do back-end

---
+++ index.js
@@ -0,0 +1,5 @@
+var tasks = [
+    {id: 1, name: "Teste", tag: "Testando", finished: false},
+    {id: 2, name: "Teste 2", tag: "Testando", finished: false},
+    {id: 3, name: "Teste 3", tag: "Testando", finished: false},
+];

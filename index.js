54a4de889c245aef5a8ddc5fa1c95db67115b885
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-07-24 17:12:03 -0300

    finalização do front end começo do back-end

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ var tasks = [
     {id: 2, name: "Teste 2", tag: "Testando", finished: false},
     {id: 3, name: "Teste 3", tag: "Testando", finished: false},
 ];
+
+//Função inicializada quando a pagina é carregada
+window.onload = function(){
+    const form = document.getElementById('create-tasks-form');
+    form.addEventListener('submit', createTask);
+}
+
+const createTask = (event) =>{
+    event.preventDefault();
+    const checkbox = getCheckboxInput
+
+}

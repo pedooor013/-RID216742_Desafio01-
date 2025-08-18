2479f306a5313eec1867d9d0cef9e3b5a1d3b67b
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-08-17 21:34:23 -0300

    fix: section de tasks arrumada para se adaptar a quantidade de conteudo contido nela

--- index.js
+++ index.js
@@ -4,14 +4,4 @@ var tasks = [
     {id: 3, name: "Teste 3", tag: "Testando", finished: false},
 ];
 
-//Função inicializada quando a pagina é carregada
-window.onload = function(){
-    const form = document.getElementById('create-tasks-form');
-    form.addEventListener('submit', createTask);
-}
 
-const createTask = (event) =>{
-    event.preventDefault();
-    const checkbox = getCheckboxInput
-
-}
